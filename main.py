54fbbf574e305033a19667006853d3804b4ee73d
天沐TNT <110798004+TianmuTNT@users.noreply.github.com>
2024-06-10 17:01:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ def getResult(s, l, i):
 
 def main():
     global query
-    query = input("\n 百度生草机 "+version+"\n 作者B站主页 https://space.bilibili.com/1674232182\n\n 输入待生草的内容\n ")
-    num = int(input(" 输入生草次数\n "))
+    query = input("\n 百度生草机 "+version+"\n 作者B站主页 https://space.bilibili.com/1674232182\n\n [输入待生草的内容]\n ")
+    num = int(input(" [输入生草次数]\n "))
     print("\n 一切就绪，开始生草！\n")
     for i in range(1, num):
         getResult(query, to[randint(0, 27)], i)
     getResult(query, "zh", num)
-    input("\n"+"="*20+"生草结果"+"="*20+"\n "+query+"\n"+"="*48+"\n\n 按下Enter键退出...\n")
+    input("\n"+"-"*20+"生草结果"+"-"*20+"\n "+query+"\n"+"-"*48+"\n\n 按下Enter键退出...\n")
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-       input("\n"+"="*20+f"\n\n 程序出现错误！\n 报错信息如下\n {e}\n 请联系作者以解决问题！\n\n"+"="*20+"\n\n按下Enter键退出...\n")
+       input("\n"+"-"*20+f"\n\n 程序出现错误！\n 报错信息如下\n {e}\n 请联系作者以解决问题！\n\n"+"-"*20+"\n\n按下Enter键退出...\n")
